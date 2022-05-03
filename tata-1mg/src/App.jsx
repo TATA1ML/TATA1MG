a51084071f6650bc89e21925d715990a9e15aeb4
ABHISHEK Mehra <abhishekmehra1245@gmail.com>
2022-05-04 01:47:01 +0530

    full Product page

---
+++ tata-1mg/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Navbar } from './components/Navbar';
+import Zandu from './components/Produxts/Zandu/Zandu';
+
+
+function App() {
+  return (
+    <div className="App">
+           <Navbar/>
+           <Zandu />
+    </div>
+  );
+}
+
+export default App;
